7c1a4ebea9c09e7bcd6ff7aab18696aad05bfd3c
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-02 16:26:40 +0530

    Second Commit

--- Day-2_intro/src/com/capgeminiIntro/CharProgram.java
+++ Day-2_intro/src/com/capgeminiIntro/CharProgram.java
@@ -1,11 +1,16 @@
 package com.capgeminiIntro;
 
+import java.util.Scanner;
+
 public class CharProgram {
 
 	public static void main(String[] args) {
+		//compile-time
 		//char x = 'a';
-		
+		Scanner s=new Scanner(System.in);
+		char x=s.next().charAt();
 		System.out.println("the character is :" +x);
+		
 	}
 
 }
