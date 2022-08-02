21dd7a9a710c2429ee16b4cebe0f77d6ea1e7c01
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-02 16:45:00 +0530

    commit

--- Day-2_intro/src/com/capgeminiIntro/BooleanProgram.java
+++ Day-2_intro/src/com/capgeminiIntro/BooleanProgram.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class BooleanProgram {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		boolean a=s.nextBoolean();
-		System.out.println(a);
+		System.out.println("Are you above 18?-");
+		Scanner sc=new Scanner(System.in);
+		boolean bn=sc.nextBoolean();
+		if(bn==true) {
+			System.out.println("You are over 18");
+			
+		}else if(bn==false) {
+			System.out.println("You are under 18");
+		}
+		sc.close();
 	}
 
 }
