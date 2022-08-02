8a0d183537c186967b563affabee3a8f7db19163
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-02 16:36:57 +0530

    commit

--- Day-2_intro/src/com/capgeminiIntro/byteintro.java
+++ Day-2_intro/src/com/capgeminiIntro/byteintro.java
@@ -1,11 +1,17 @@
 package com.capgeminiIntro;
 
+import java.util.Scanner;
 
 //program for byte data-type
 public class byteintro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the number you want:");
+		  Byte num=input.nextByte();
+		  	System.out.println("output value: "+(num *(num+2))/2);
+		  	input.close();
+		  
 
 	}
 
