4a6088fda1928107a204e036e69b2ff642936eba
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-02 16:29:38 +0530

    second commit

--- Day-2_intro/src/com/capgeminiIntro/FloatProgram.java
+++ Day-2_intro/src/com/capgeminiIntro/FloatProgram.java
@@ -3,8 +3,10 @@ package com.capgeminiIntro;
 public class FloatProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float num1=5.5f;
+		float num2=5f;
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
 	}
 
 }
