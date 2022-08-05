d2913d6794cd91a55be5b6ee873366a657909c65
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-05 15:29:00 +0530

    Commit

---
+++ Day-5_Inheritance,Scanner and BufferedReader class/src/com/cg/scanbuf/ScanProgram.java
@@ -0,0 +1,15 @@
+package com.cg.scanbuf;
+
+import java.util.Scanner;
+
+public class ScanProgram {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println(a);
+		
+		s.close();
+	}
+
+}
