589042ddecd1cecef523ee5e9ebebaa23e176e0c
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-06 17:12:21 +0530

    Commit

---
+++ Day-6(b)Assignement/src/com/cap/assignment/DoWhileProgram.java
@@ -0,0 +1,16 @@
+package com.cap.assignment;
+
+public class DoWhileProgram {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do 
+		{
+			System.out.println("hello "+i);
+			i++;
+		}
+		while(i <=3);
+
+	}
+
+}
---
+++ Day-6(b)Assignement/src/com/cap/assignment/EnhancedForLoopProgram.java
@@ -0,0 +1,14 @@
+package com.cap.assignment;
+
+public class EnhancedForLoopProgram {
+
+	public static void main(String[] args) {
+		int[] arr = {5, 2, 3, 4};
+		int i = 0;
+		for(int item : arr)
+			System.out.println(item + " " + i++);
+		
+
+	}
+
+}
