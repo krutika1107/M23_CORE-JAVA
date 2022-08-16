e5527b0179d959b6672dc7f1f99f7bdaca491e78
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-16 14:25:05 +0530

    commit

---
+++ Day_11-String, StringBuffer/src/com/cap/string/Example2.java
@@ -0,0 +1,14 @@
+package com.cap.string;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=new String(sc.nextLine());
+		System.out.println(str);
+		sc.close();
+	}
+
+}
