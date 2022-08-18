a6de4ca867b8f117c3e2f2c22525cbaf2340eb19
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-18 15:26:12 +0530

    commit

---
+++ Day-13_Collection-ArrayList-LinkedList-Vector/src/com/cg/list/Vector.java
@@ -0,0 +1,14 @@
+package com.cg.list;
+import java.util.Vector;
+public class Vector {
+
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add(11);
+		v.add("Krutika");
+		System.out.println(v);
+		
+
+	}
+
+}
