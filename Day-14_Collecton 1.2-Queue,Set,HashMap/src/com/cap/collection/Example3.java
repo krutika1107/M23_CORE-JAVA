532d94df039360cc57dcfeca4d10af5ecf78129e
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-20 12:56:54 +0530

    commit

---
+++ Day-14_Collecton 1.2-Queue,Set,HashMap/src/com/cap/collection/Example3.java
@@ -0,0 +1,19 @@
+package com.cap.collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		LinkedHashSet s=new LinkedHashSet();
+		s.add(11);
+		s.add(null);
+		s.add(11);
+		System.out.println(s);
+
+
+	}
+
+}
