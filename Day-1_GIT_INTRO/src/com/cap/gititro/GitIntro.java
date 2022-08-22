b69396f208d31f50a9a0dd7ba4466cd0f767535c
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-08-22 16:57:39 +0530

    commit

---
+++ Day-1_GIT_INTRO/src/com/cap/gititro/GitIntro.java
@@ -0,0 +1,12 @@
+package com.cap.gititro;
+//demo for to connect eclipse with GitHub
+public class GitIntro {
+
+	public static void main(String[] args) {
+		//print the line
+		System.out.println("Welcome to Capgemini C2TC Program");
+		
+
+	}
+
+}
