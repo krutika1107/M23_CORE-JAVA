f374425e7b7df029325907011709d680cae3b4d0
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-09-07 13:34:27 +0530

    commit

---
+++ Day-26_Banking_Applications/src/com/cg/framework/BankFactory.java
@@ -0,0 +1,15 @@
+package com.cg.framework;
+
+public class BankFactory {
+
+	public SavingAccount getNewSavingAccount(int accNo, String accNm, float accBal, boolean isSalaried) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CurrentAccount getNewCurrentAccount(int accNo, String accNm, float accBal, float creditLimit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
